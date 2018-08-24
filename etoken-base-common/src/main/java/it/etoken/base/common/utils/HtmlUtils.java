e07a7a4005bf89f875b0806c27afc324f5952066
abill <48088936@qq.com>
2018-08-24 14:23:23 +0800

    Fixed #9 解决资讯没有换行处理问题

--- etoken-base-common/src/main/java/it/etoken/base/common/utils/HtmlUtils.java
+++ etoken-base-common/src/main/java/it/etoken/base/common/utils/HtmlUtils.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class HtmlUtils {
@@ -21,6 +20,7 @@ public class HtmlUtils {
 		}
 		PrintStream printStream = null;
 		try {
+			html = nr2html(html);
 			printStream = new PrintStream(new FileOutputStream(f));
 			printStream.println(template.replace("${content}", html));
 			return filePath;
@@ -48,6 +48,7 @@ public class HtmlUtils {
 				eosprice="55.7";
 				System.out.println(eosprice);
 	        }
+			content = nr2html(content);
 			printStream = new PrintStream(new FileOutputStream(f));
 			printStream.println(String.format(template, title,content,eosprice));
 			return filePath;
@@ -72,6 +73,7 @@ public class HtmlUtils {
 			if(eosprice==null || "".equals(eosprice)){
 				eosprice="55.7";
 	        }
+			content = nr2html(content);
 			printStream = new PrintStream(new FileOutputStream(f,false));
 			printStream.println(String.format(template, title,content,eosprice));
 		} catch (Exception e) {
@@ -82,22 +84,38 @@ public class HtmlUtils {
 		}
 	}
 	//修改资讯的已经产生的html模板
-		public static void regemHtmlForInformation(String html,String filePath) throws Exception {
-			File f = new File(filePath);
-			if (!f.getParentFile().exists()) {
-				f.getParentFile().mkdirs();
-			}
-			PrintStream printStream = null;
-			try {
-				printStream = new PrintStream(new FileOutputStream(f));
-				printStream.println(template.replace("${content}", html));
-			} catch (Exception e) {
-				throw e;
-			} finally {
-				if (printStream != null)
-					printStream.close();
-			}
+	public static void regemHtmlForInformation(String html,String filePath) throws Exception {
+		File f = new File(filePath);
+		if (!f.getParentFile().exists()) {
+			f.getParentFile().mkdirs();
 		}
+		PrintStream printStream = null;
+		try {
+			printStream = new PrintStream(new FileOutputStream(f));
+			html = nr2html(html);
+			printStream.println(template.replace("${content}", html));
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (printStream != null)
+				printStream.close();
+		}
+	}
 	
+	public static String nr2html(String str) {
+		return str.replaceAll("[\n\r]", "<br>"); 
+	}
 	
+	public static void main(String[] args) {
+		String html = "<p>今日早间BTC小幅上涨，突破6500美元。BTC现报价6500.68美元，涨幅1.09%，主流币种普涨。&nbsp;\n" + 
+				"1. 全球数字货币市场总市值现为2111.61亿美元，24h成交量为103.32亿美元。&nbsp;\n" + 
+				"2. BTC现全球均价6526美元，三大主流交易所中，火币上价格为6500.68美元，OKEx上价格为6502.36美元，币安上价格为6505.02美元。&nbsp;\n" + 
+				"3. 据《币世界》行情，其他主流币种表现如下：BCH暂报529.28美元（+1.70%）、ETC暂报12.47美元（+1.30%）、ETH暂报274.72美元（+1.02%）、LTC暂报56.87美元（+2.74%）、EOS暂报4.87美元（+1.37%）。&nbsp;\n" + 
+				"4. 据《币世界》行情显示，市值前百币种24小时内行情跌多涨少。涨幅前三为：BCD(+42.43%)、GAS（+21.11%）、WTC（+17.46%）。跌幅前三为：NAS（-6.05%）、GXC（-2.81%）、EMC（-2.67%）。&nbsp;\n" + 
+				"5. 概念板块24小时内仅两只下跌，涨幅前三分别为：IFO概念（+12.46%）、超级算力（+6.18%）、社交通讯（+4.32%）；下跌的分别为：2018世界杯（-1.69%）、平台币（-0.14%）。&nbsp;\n" + 
+				"6. Cboe比特币9月合约实时报价6495美元，CME比特币8月合约实时报价6505美元。</p>";
+		html = nr2html(html);
+		
+		System.out.println(html);
+	}
 }
