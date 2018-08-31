8e0785444e923b28fa894340c22c61f638085afa
abill <48088936@qq.com>
2018-08-31 12:29:16 +0800

    恢复SpringBoot版本到1.5.15.RELEASE

--- etoken-base-cache/src/main/java/it/etoken/base/cache/RedisCacheConfig.java
+++ etoken-base-cache/src/main/java/it/etoken/base/cache/RedisCacheConfig.java
@@ -1,6 +1,8 @@
 package it.etoken.base.cache;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +12,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -64,19 +64,11 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
     }
 	
 
-//    @Bean 
-//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-//        Map<String,Long> expiresMap=new HashMap<>();
-//        return cacheManager;
-//    }
-	
-	@Bean 
-	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
-        return RedisCacheManager
-                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
-                .cacheDefaults(redisCacheConfiguration).build();
+    @Bean 
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        Map<String,Long> expiresMap=new HashMap<>();
+        return cacheManager;
     }
  
 }
