47e492a6d5588e13c2445a491dd1fea14ab52a30
abill <48088936@qq.com>
2018-08-31 11:50:46 +0800

    升级redis封装

--- etoken-base-cache/src/main/java/it/etoken/base/cache/RedisCacheConfig.java
+++ etoken-base-cache/src/main/java/it/etoken/base/cache/RedisCacheConfig.java
@@ -1,8 +1,6 @@
 package it.etoken.base.cache;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +10,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -64,11 +64,19 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
     }
 	
 
-    @Bean 
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-        Map<String,Long> expiresMap=new HashMap<>();
-        return cacheManager;
+//    @Bean 
+//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//        Map<String,Long> expiresMap=new HashMap<>();
+//        return cacheManager;
+//    }
+	
+	@Bean 
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        return RedisCacheManager
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration).build();
     }
  
 }
