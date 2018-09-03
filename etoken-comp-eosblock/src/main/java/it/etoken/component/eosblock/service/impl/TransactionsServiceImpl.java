888d7c5572e51bbcbccebdc23ee82816d76269c8
alisa <alisa@qq.com>
2018-09-03 18:39:21 +0800

    Fixed #38

--- etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/impl/TransactionsServiceImpl.java
+++ etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/impl/TransactionsServiceImpl.java
@@ -599,7 +599,7 @@ public class TransactionsServiceImpl implements TransactionsService{
 	public List<JSONObject> getEosTransactionRecord(int start, int count, String account, String sort, String token,
 			String contract) {
 		Criteria accountCriteria = null;
-		if(null != contract || !"".equals(contract)){
+		if(!"".equals(contract)&&null != contract){
 			accountCriteria = Criteria.where("actions.account").is(contract);
 		}
 		Criteria[] actorCriterias = new Criteria[3];
@@ -611,227 +611,96 @@ public class TransactionsServiceImpl implements TransactionsService{
 		actorCriteria.orOperator(actorCriterias);
 		
 		Criteria codeCriteria = new Criteria();
-//		Object[] actionstNames = new Object[] { "delegatebw", "sellram","undelegatebw"};
-		Pattern pattern=Pattern.compile("^.*"+token+".*$", Pattern.CASE_INSENSITIVE);
-		codeCriteria.orOperator(Criteria.where("actions.data.quantity").regex(pattern),
-				Criteria.where("actions.name").is("delegatebw"),
-				Criteria.where("actions.name").is("sellram"),
-				Criteria.where("actions.name").is("undelegatebw")
-//				Criteria.where("actions.name").is("newaccount")
-//                Criteria.where("actions.data.stake_cpu_quantit").regex(pattern),
-//                Criteria.where("actions.data.quant").regex(pattern),
-               );
-		
-//		Criteria blockIdCriteria =Criteria.where("block_id").exists(true);
-		
-		
+		if(!"".equals(token)&&null != token) {
+			Pattern pattern=Pattern.compile("^.*"+token+".*$", Pattern.CASE_INSENSITIVE);
+			codeCriteria.orOperator(Criteria.where("actions.data.quantity").regex(pattern),
+					Criteria.where("actions.name").is("delegatebw"),
+					Criteria.where("actions.name").is("sellram"),
+					Criteria.where("actions.name").is("undelegatebw")
+	               );
+		}
 		Criteria criteria = new Criteria();
-		if(accountCriteria!=null) {
+		if(accountCriteria!=null&& codeCriteria!=null) {
 		   criteria.andOperator(accountCriteria,actorCriteria,codeCriteria);
 		   System.out.println(criteria.getCriteriaObject());
 		}else {
-		   criteria.andOperator(actorCriteria,codeCriteria);
+		   criteria=actorCriteria;
 		   System.out.println(criteria.getCriteriaObject());
 		}
-		
-		Map<String, String> existMap = new HashMap<String, String>();
-		List<JSONObject> list=new ArrayList<JSONObject>();
-		boolean haveList = true;
-		int countN = 0;
-		do {
-			    Query query = new Query(criteria);
-			    if(sort.equals("desc")) {
-			       query = query.with(new Sort(new Order(Direction.DESC, "createdAt")));
-			    }
-			    if(sort.equals("asc")) {
-			       query = query.with(new Sort(new Order(Direction.ASC, "createdAt")));
-			    }
-			    query = query.limit(count);
-			    query = query.skip(start);
-			    List<BasicDBObject> transactionsList = mongoTemplate.find(query, BasicDBObject.class, "transactions");
-				
-			    if(null == transactionsList || transactionsList.isEmpty()) {
-					haveList = false;
-					break;
+		 Query query = new Query(criteria);
+		 if(sort.equals("desc")) {
+		       query = query.with(new Sort(new Order(Direction.DESC, "createdAt")));
+		 }
+		 if(sort.equals("asc")) {
+		       query = query.with(new Sort(new Order(Direction.ASC, "createdAt")));
+		 }
+		 query = query.limit(count);
+		 query = query.skip(start);
+		 List<BasicDBObject> transactionsList = mongoTemplate.find(query, BasicDBObject.class, "transactions");	    
+		 List<JSONObject> list=new ArrayList<JSONObject>();
+		 for (BasicDBObject thisBasicDBObject :transactionsList) {
+				String transactionId=thisBasicDBObject.getString("trx_id");
+				String blockNum=thisBasicDBObject.getString("block_num");
+				Date  blockTime=thisBasicDBObject.getDate("createdAt");
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	        	Long times = 0l;
+				BigDecimal price = BigDecimal.ZERO;
+				try {
+					times = sdf.parse(sdf.format(blockTime)).getTime();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			    start=count+start;
-				for (BasicDBObject thisBasicDBObject :transactionsList) {
-					String transactionId=thisBasicDBObject.getString("trx_id");
-					if (existMap.containsKey(transactionId)) {
-						continue;
+				price = this.getRamPriceByTimes(times);
+				BasicDBList actions = (BasicDBList) thisBasicDBObject.get("actions");
+				Object[] thisActions = actions.toArray();
+				for(Object thisAction : thisActions) {
+					BasicDBObject action = (BasicDBObject)thisAction;
+					BasicDBObject data = (BasicDBObject) action.get("data");
+					String actionName = action.getString("name");
+					String memo= data.getString("memo");
+					if(null==memo) {
+						memo="";
 					}
-					String blockNum=thisBasicDBObject.getString("block_num");
-					if(blockNum==null || blockNum.isEmpty()) {
-						Date time=thisBasicDBObject.getDate("createdAt");
-						Date newDate=new Date();
-						if(newDate.getTime()-time.getTime()>10*60*1000) {
-							continue;
-						}
-						Query queryBlockNum = new Query(Criteria.where("trx_id").is(transactionId));
-						queryBlockNum = queryBlockNum.addCriteria(Criteria.where("block_id").exists(true));
-						queryBlockNum = queryBlockNum.with(new Sort(new Order(Direction.DESC, "updatedAt")));
-						queryBlockNum = queryBlockNum.limit(1);
-						List<BasicDBObject> existTransactionsList = mongoTemplate.find(queryBlockNum, BasicDBObject.class, "transactions");
-						if (null != existTransactionsList && !existTransactionsList.isEmpty()) {
-							thisBasicDBObject=existTransactionsList.get(0);
-							blockNum=thisBasicDBObject.getString("block_num");
-						}
+					String from=data.getString("from");
+					if(null==from) {
+						from="";
 					}
-					String type="";
-					String to="";
-					String from="";
-					String quantity="0.0";
-					String memo="";
-					String description="";
-					String code_new="";
-					String _id=thisBasicDBObject.getString("_id");
-					Date  blockTime=thisBasicDBObject.getDate("createdAt");
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		        	Long times = 0l;
-					BigDecimal price = BigDecimal.ZERO;
-					try {
-						times = sdf.parse(sdf.format(blockTime)).getTime();
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					String account1=action.getString("account");
+					if(null==account1) {
+						account1="";
 					}
-					price = this.getRamPriceByTimes(times);
-					BasicDBList actions = (BasicDBList) thisBasicDBObject.get("actions");
-					Object[] thisActions = actions.toArray();
-					for(Object thisAction : thisActions) {
-						BasicDBObject action = (BasicDBObject)thisAction;
-						BasicDBObject data = (BasicDBObject) action.get("data");
-						String actionName = action.getString("name");
-						 if(actionName.equalsIgnoreCase("transfer")) {
-				            	description="转账";
-				            	to=data.getString("to");
-				            	from=data.getString("from");
-				            	if(from.equals(account)) {
-				 			    	type="转出";
-				 			    }
-				 			    if(to.equals(account)) {
-				 			    	type="转入";
-				 			    }
-				            	quantity=data.getString("quantity");
-				             	String[] quantity_arra= quantity.split(" ");
-				             	quantity=quantity_arra[0];
-				             	code_new=quantity_arra[1];
-				            	memo=data.getString("memo");
-				            }else if(actionName.equalsIgnoreCase("newaccount")) {
-				            	memo="";
-				            	from=data.getString("creator");
-				            	to=data.getString("name");
-				            	type="创建账号";
-				            	description="创建账号";
-				            	quantity="0.0";
-				            }else if(actionName.equalsIgnoreCase("delegatebw")) {
-				            	memo="";
-				            	from=data.getString("from");
-				            	to=data.getString("receiver");
-				            	String stake_net_quantity=data.getString("stake_net_quantity");
-				            	String[] stake_net_quantity_array= stake_net_quantity.split(" ");
-				            	String stake_cpu_quantity=data.getString("stake_cpu_quantity");
-				            	String[] stake_cpu_quantity_array= stake_cpu_quantity.split(" ");
-				            	BigDecimal netQuantity= new  BigDecimal(stake_net_quantity_array[0].trim());
-				            	BigDecimal cpuQuantity= new  BigDecimal(stake_cpu_quantity_array[0].trim());
-				            	BigDecimal quantitys = null; 
-				            	code_new=stake_cpu_quantity_array[1];
-				            	type="转出";
-				            	description="抵押";
-				            	quantitys=netQuantity.add(cpuQuantity);
-				            	quantity=quantitys.toString();
-				            }else if(actionName.equalsIgnoreCase("undelegatebw")) {
-				            	memo="";
-				            	from=data.getString("from");
-				            	to=data.getString("receiver");
-				            	String unstake_net_quantity=data.getString("unstake_net_quantity");
-				            	String[] unstake_net_quantity_array= unstake_net_quantity.split(" ");
-				            	String unstake_cpu_quantity=data.getString("unstake_cpu_quantity");
-				            	String[] unstake_cpu_quantity_array= unstake_cpu_quantity.split(" ");
-				            	BigDecimal netQuantity= new  BigDecimal(unstake_net_quantity_array[0]);
-				            	BigDecimal cpuQuantity= new  BigDecimal(unstake_cpu_quantity_array[0]);
-				            	code_new=unstake_cpu_quantity_array[1];
-				            	BigDecimal quantitys = null; 
-				            	type="转入";
-			            		description="赎回";
-			            		quantitys=netQuantity.add(cpuQuantity);
-				            	quantity=quantitys.toString();
-				            }else if(actionName.equalsIgnoreCase("buyram")) {
-				            	description="内存购买";
-				            	memo="";
-				            	type="转出";
-				            	from=data.getString("payer");
-				            	to=data.getString("receiver");
-				            	quantity=data.getString("quant");
-				            	String[] quantity_arra= quantity.split(" ");
-				             	quantity=quantity_arra[0];
-				             	code_new=quantity_arra[1];
-				            }else if(actionName.equalsIgnoreCase("sellram")) {
-				            	Object[] obj=new Object[1];
-				            	obj[0]=transactionId;
-				            	Map<String, String> priceMap=transactionsService.findSellRamExactPrice(obj);
-				        		price=new BigDecimal(priceMap.get(transactionId));
-				            	description="内存出售";
-				            	memo="";
-				            	type="转入";
-				            	to="";
-				            	from=data.getString("account");
-				            	Long bytes = data.getLong("bytes");
-								BigDecimal bytesK = BigDecimal.valueOf(bytes).divide(BigDecimal.valueOf(1024l), 2, BigDecimal.ROUND_HALF_UP);
-								BigDecimal eos_qty = bytesK.multiply(price);
-								eos_qty.setScale(4, BigDecimal.ROUND_HALF_UP);
-								quantity=eos_qty.toString();
-								code_new=token;
-				            }else if(actionName.equalsIgnoreCase("issue")) {
-				            	description="发行";
-				            	memo="";
-				            	type="转入";
-				            	to=data.getString("to").trim();
-				            	if(!account.equals(to)) {
-				            		continue;
-				            	}
-				            	from="";
-				            	quantity=data.getString("quantity");
-				            	String[] quantity_arra= quantity.split(" ");
-				             	quantity=quantity_arra[0];
-				             	code_new=quantity_arra[1];
-				            }else {
-				            	continue;
-							}
-							    JSONObject jsonObjects = new JSONObject();
-							    jsonObjects.put("_id", _id);
-							    jsonObjects.put("quantity", quantity+code_new);//code_new是单位如EOS,MSP	
-								jsonObjects.put("description", description);
-								jsonObjects.put("memo",memo);
-								jsonObjects.put("from", from);
-								jsonObjects.put("blockNum", blockNum);
-								jsonObjects.put("blockTime", sdf.format(blockTime));
-								jsonObjects.put("to", to);
-								jsonObjects.put("type", type);
-								jsonObjects.put("transactionId", transactionId);
-								if (null == token || token.isEmpty()) {
-								jsonObjects.put("code", code_new);	
-								}else {
-								jsonObjects.put("code", token);		
-								}
-								existMap.put(transactionId, transactionId);
-						        list.add(jsonObjects);
-						    	countN++;
-						    	if(transactionsList.size()>count) {
-						    		if(countN == count) {
-										existMap.clear();
-										return list;
-									}
-						    	}else {
-						    		if(countN==transactionsList.size()) {
-						    			existMap.clear();
-										return list;
-						    		}
-						    	}	
-					  }
-			    }
-		} while (haveList);
-		existMap.clear();
-		return list;
-	}   
+					String quantity=data.getString("quantity");
+					String count1="";
+					String symbol= "";
+					if(null==quantity) {
+						quantity="";
+					}else {
+						String[] quantity_arra= data.getString("quantity").split(" ");
+					    count1=quantity_arra[0];
+						symbol= quantity_arra[1];
+					}
+					String bytes=data.getString("bytes");
+					if(null==bytes) {
+						bytes="";
+					}
+					JSONObject jsonObjects = new JSONObject();
+				    jsonObjects.put("quantity", quantity);//code_new是单位如EOS,MSP	
+					jsonObjects.put("memo",memo);
+					jsonObjects.put("from", from);
+					jsonObjects.put("blockNum", blockNum);
+					jsonObjects.put("timestamp", sdf.format(blockTime));	
+					jsonObjects.put("transactionId", transactionId);
+					jsonObjects.put("account", account1);
+					jsonObjects.put("name", actionName);
+					jsonObjects.put("data", data);
+					jsonObjects.put("count", count1);
+					jsonObjects.put("symbol",symbol);	
+					jsonObjects.put("ram_price", price);	
+					jsonObjects.put("bytes", bytes);	
+			        list.add(jsonObjects);       
+				}	
+			}
+			return list;
+	}     
 }
