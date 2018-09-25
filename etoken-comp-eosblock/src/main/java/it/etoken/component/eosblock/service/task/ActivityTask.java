bb8bf1e3c057e4f93050cb9207c1da042aacc268
abill <48088936@qq.com>
2018-09-25 21:45:02 +0800

    活动job打开

--- etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/task/ActivityTask.java
+++ etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/task/ActivityTask.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import it.etoken.component.eosblock.service.ActivityService;
 
-//@Component
+@Component
 public class ActivityTask {
 
 	@Autowired
@@ -15,22 +15,22 @@ public class ActivityTask {
 	// 获取购买币的用户
 	@Scheduled(cron = "*/10 * * * * ?")
 	public void getBuyUser() {
-		System.out.println("开始获取交易用户信息...");
+		System.out.println("活动-------------------------------------------------------开始获取交易用户信息...");
 		activityService.getAllBuyUsers();
-		System.out.println("获取交易用户信息结束.");
+		System.out.println("活动-------------------------------------------------------获取交易用户信息结束.");
 	}
 	
 	@Scheduled(cron = "*/5 * * * * ?")
 	public void updateStatus() {
-		System.out.println("开始更新活动状态...");
+		System.out.println("活动-------------------------------------------------------开始更新活动状态...");
 		activityService.updateStatus();
-		System.out.println("更新活动状态结束.");
+		System.out.println("活动-------------------------------------------------------更新活动状态结束.");
 	}
 	
 	@Scheduled(cron = "0 */15 * * * ?")
 	public void transfer2WinAndLuckyUser() {
-		System.out.println("开始转账给用户...");
+		System.out.println("活动-------------------------------------------------------开始转账给用户...");
 		activityService.transfer2WinAndLuckyUser();
-		System.out.println("转账给用户结束");
+		System.out.println("活动-------------------------------------------------------转账给用户结束");
 	}
 }
