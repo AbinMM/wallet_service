41aa6fe1147b1710a1bd7c24dc405223e6c8ea8e
alisa <alisa@qq.com>
2018-10-22 15:07:33 +0800

    Fixed #77 获取内存大户排行逻辑改变

--- etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/RamLargeRankService.java
+++ etoken-comp-eosblock/src/main/java/it/etoken/component/eosblock/service/RamLargeRankService.java
@@ -10,4 +10,12 @@ public interface RamLargeRankService {
 	
 	//获取内存大户排行并且放到mongodb中
 	public void getLargeRank();
+	
+	
+	//获取半个小时大单交易的用户并查询他们的内存值跟内存前20的内存值比较如果大于并且放到mongodb中
+	public void getNewLargeRank();
+	
+	//修改已经存在大户排名中的大户的内存量
+	public void updateLargeRank();
+
 }
